3321c3db72dfc7bbdb1879a1784b4be2bdc3a1c5
fedora <hdkef11@gmail.com>
2021-11-08 12:40:14 +0700

    add ppe basic dashboard

--- assets/js/ppe-dashboard.js
+++ assets/js/ppe-dashboard.js
@@ -22,11 +22,31 @@ function initWS(){
             let msg = jsonData.msg
             switch (jsonData.type){
                 case "initPPEFromServer":
-                    alert(JSON.stringify(data))
+                    populateInbox(data)
                     break
             }
         }
     })
+}
+
+//to populate or create new row of inbox
+function populateInbox(inboxArray){
+    let tableBody = document.getElementById("table-body-inbox")
+    for (let i =0;i < inboxArray.length;i++){
+        let id = inboxArray[i].ppe_inbox_id
+        let priority = inboxArray[i].WorkRequest.work_request_priority
+        let est_cost = inboxArray[i].ppe_inbox_est_cost
+        let date_created = inboxArray[i].WorkRequest.work_request_date_created
+        let task = inboxArray[i].WorkRequest.work_request_task
+        let location = inboxArray[i].WorkRequest.work_request_location
+        let equipment = inboxArray[i].WorkRequest.work_request_equipment
+        let newRow = document.createElement("tr")
+        newRow.id = `inbox-${id}`
+        let newRowInnerHTML = `<td>${id}</td><td>${priority}</td><td>${date_created}</td><td>${task}</td><td>${est_cost}</td><td>${location}</td><td>${equipment}</td><td><button>Detail</button></td>`
+        newRow.innerHTML = newRowInnerHTML
+        inboxMap.set(id,inboxArray[i])
+        tableBody.appendChild(newRow)
+    }
 }
 
 initWS()
